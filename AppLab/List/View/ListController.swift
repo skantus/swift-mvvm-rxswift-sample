4b4e59b32e7df23443eb20b57b0d43a64fa771ef
skantus <drskantus@hotmail.com>
2020-07-28 22:43:47 +0200

    Update structure

---
+++ AppLab/List/View/ListController.swift
@@ -0,0 +1,66 @@
+//
+//  PostsListViewController.swift
+//  AppLab
+//
+//  Created by Alejo Castaño on 28/07/2020.
+//  Copyright © 2020 Alejo Castaño. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    var listTableView: UITableView!
+    var listViewModel = ViewModelList()
+    
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+
+        listTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        listTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        listTableView.dataSource = self
+        listTableView.delegate = self
+     
+        self.view.addSubview(listTableView)
+        
+        listViewModel.retreiveDataList()
+        bindList()
+    }
+    
+    private func bindList() {
+        listViewModel.refreshData = { [weak self] () in
+            DispatchQueue.main.sync {
+                self?.listTableView.reloadData()
+            }
+        }
+    }
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listViewModel.dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath as IndexPath)
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellId)
+        
+        let object = listViewModel.dataArray[indexPath.row]
+
+        cell.textLabel!.text = object.title
+        cell.textLabel!.numberOfLines = 2
+        cell.detailTextLabel?.text = object.body
+        
+        return cell
+    }
+}
--- AppLab/ViewController.swift
+++ AppLab/ViewController.swift
@@ -7,15 +7,9 @@
 //
 
 import UIKit
-import TinyConstraints
 
 class ViewController: UIViewController {
 
-    var listViewModel = ViewModelList()
-    var listTableView: UITableView!
-    
-    private let cellId = "cellId"
-    
     lazy var activityIndicator: UIActivityIndicatorView = {
         let view = UIActivityIndicatorView(style: .medium)
         return view
@@ -25,80 +19,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupNavigation()
-        setupTableView()
         setupViews()
     }
     
     fileprivate func setupNavigation() {
-        let resetBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetBarButtonItemTapped))
+        let resetBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(leftBarButtonItemTapped))
         
-        let fetchBarButtonItem = UIBarButtonItem(title: "Fetch", style: .plain, target: self, action: #selector(fetchBarButtonItemTapped))
+        let fetchBarButtonItem = UIBarButtonItem(title: "List", style: .plain, target: self, action: #selector(rightBarButtonItemTapped))
         
         navigationItem.setLeftBarButton(resetBarButtonItem, animated: false)
         navigationItem.setRightBarButton(fetchBarButtonItem, animated: false)
     }
     
-    @objc fileprivate func resetBarButtonItemTapped() {
-        activityIndicator.isHidden = true
-        navigationItem.title = "Reset"
-        
-        self.listTableView.reloadData()
-    }
+    @objc fileprivate func leftBarButtonItemTapped() {}
     
-    @objc fileprivate func fetchBarButtonItemTapped() {
-        activityIndicator.isHidden = false
-        navigationItem.title = "Fetching..."
-
-        activityIndicator.startAnimating()
-        listViewModel.retreiveDataList()
-        bindList()
+    @objc fileprivate func rightBarButtonItemTapped() {
+        self.navigationController?.pushViewController(ListViewController(), animated: true)
     }
     
     fileprivate func setupViews() {
         view.addSubview(activityIndicator)
-        activityIndicator.centerXToSuperview()
         view.backgroundColor = .white
-    }
-    
-    func setupTableView() {
-      let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
-      let displayWidth: CGFloat = self.view.frame.width
-      let displayHeight: CGFloat = self.view.frame.height
-
-      listTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
-      listTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-      listTableView.dataSource = self
-      listTableView.delegate = self
-      
-      self.view.addSubview(listTableView)
-    }
-    
-    private func bindList() {
-        listViewModel.refreshData = { [weak self] () in
-            DispatchQueue.main.sync {
-                self?.listTableView.reloadData()
-                self?.activityIndicator.stopAnimating()
-                self?.navigationItem.title = "Loaded data!"
-            }
-        }
-    }
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listViewModel.dataArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath as IndexPath)
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellId)
-        
-        let object = listViewModel.dataArray[indexPath.row]
-
-        cell.textLabel!.text = object.title
-        cell.textLabel!.numberOfLines = 2
-        cell.detailTextLabel?.text = object.body
-        
-        return cell
     }
 }
